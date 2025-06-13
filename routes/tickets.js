5d140b875892ff17ad1bec038477a8d898ed161b
teknikinfo598 <teknik.info598@gmail.com>
2025-06-13 23:30:32 +0300

    Delete routes directory

--- routes/tickets.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getTickets, createTicket } = require('../controllers/ticketController');
-
-router.get('/', getTickets);
-router.post('/', createTicket);
-
-module.exports = router;
